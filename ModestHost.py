a273e09bfd4bd276e8b77e5a3b77713ec61c51b8
rafalkaron <rafalkaron@gmail.com>
2020-02-15 14:32:56 +0100

    ModestHost v0.2

--- ModestHost.py
+++ ModestHost.py
@@ -1,11 +1,11 @@
 #coding: utf-8
 """
-    SSGenie (Static Site Genie)
-    ******************************************************
+    ModestHost (Codename: SSGenie)
+    *********************************************************
 
-    Quickly host files locally.
+    Quickly host files from the ModestHost directory.
 
-    ******************************************************
+    *********************************************************
 """
 
 import http.server
@@ -14,10 +14,9 @@ import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-#import webbrowser
 import threading
 
-__version__ = "0.1"
+__version__ = "0.2"
 __author__ = "Rafał Karoń <rafalkaron@gmail.com>"
 
 
@@ -28,8 +27,6 @@ def open_chrome_localhost():
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     driver.get("localhost:8000")
-    #webbrowser.register("chrome", None,webbrowser.BackgroundBrowser("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"))
-    #webbrowser.get("chrome").open_new("localhost:8000")
 
 def start_server():
     PORT = 8000
@@ -37,13 +34,15 @@ def start_server():
     _script_filename = os.path.basename(__file__)
     _script_directory = _script_filepath.replace(_script_filename, "").replace("\\", "/")
     socketserver.TCPServer(("localhost", PORT), http.server.SimpleHTTPRequestHandler).serve_forever()
-
+"""
 def stop_server():
     _exit_prompt = input("To finish, press [Enter]")
     if _exit_prompt:
         exit(0)
+"""
+def main():
+    t1 = threading.Thread(target=start_server)
+    t1.start()
+    open_chrome_localhost()
 
-t1 = threading.Thread(target=start_server)
-t1.start()
-
-open_chrome_localhost()
+main()
