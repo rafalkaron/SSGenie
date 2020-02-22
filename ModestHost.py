d2b4e14de200b0508cd2341d62b7c3d673ed4d82
rafalkaron <rafalkaron@gmail.com>
2020-02-22 15:41:51 +0100

    to mac

--- ModestHost.py
+++ ModestHost.py
@@ -23,31 +23,9 @@ __author__ = "Rafał Karoń <rafalkaron@gmail.com>"
 address = "localhost"
 
 if getattr(sys, 'frozen', False):
-    global bundle_dir
-    bundle_dir = sys._MEIPASS
-else:
-    bundle_dir = os.path.dirname(os.path.abspath(__file__))
-
-print( 'bundle dir is', bundle_dir )
-print( 'sys.argv[0] is', sys.argv[0] )
-print( 'sys.executable is', sys.executable )
-print( 'os.getcwd is', os.getcwd() )
-
-def current_dir():
-    global host_dir
-    host_dir = os.getcwd()
-    os.chdir(host_dir)
-
-def enter_dir():
-    global host_dir
-    try:
-        host_dir = input("Enter the full path to the directory that you want to host: ")
-        os.chdir(host_dir)
-    except:
-        print("Try again!") #add an example for each system
-        enter_dir()
-
-
+    application_path = os.path.dirname(sys.executable)
+elif __file__:
+    application_path = os.path.dirname(__file__)
 
 def server():
     httpd = socketserver.TCPServer((address, PORT), http.server.SimpleHTTPRequestHandler)
@@ -56,7 +34,7 @@ def server():
 def start_server():
     global PORT
     PORT = 8000
-    #print("\n>>> Hosting files from " + host_dir + " on " + address + ":" + str(PORT)+"\n")
+    print("\n>>> Hosting files from " + application_path + " on " + address + ":" + str(PORT)+"\n")
     while True:
         try:
             server()
@@ -66,7 +44,6 @@ def start_server():
         finally:
             continue
         break
-
 
 def open_chrome_localhost():
     print(">>> Opening a default Google Chrome instance on "+ address + ":" + str(PORT))
@@ -78,13 +55,8 @@ def open_chrome_localhost():
     driver.get(address +":"+str(PORT))
 
 def main():
-    if os.name=="nt":
-        current_dir()
-    if os.name=="posix":
-        os.chdir(bundle_dir)
-
+    os.chdir(application_path)
     t1 = threading.Thread(target=start_server)
     t1.start()
     open_chrome_localhost()
- 
 main()
