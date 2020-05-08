0978daa59a4dd535d835602c09b945e65d4e3241
rafalkaron <rafalkaron@gmail.com>
2020-05-08 21:08:29 +0200

    update

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@
 """
 Quickly host files from the ModestHost directory on a localhost web server.
 """
-### Compile this as bundled executable and pack it up with platypus. check theprevent closing option and add run.sh.
+# Compile by using pyinstaller with the --onedir argument. Then, add the files from the compiled directory to the Platypus bundle and use the run.sh file as the Platypus script.
 import http.server
 import socketserver
 import webbrowser
@@ -24,14 +24,14 @@ def exe_dir():
         exe_path = os.path.dirname(__file__)
     return exe_path
 
-def start_localhost():
+def start_web_server():
     global port
     port = 7999
     while True:
         try:
             port +=1
             httpd = socketserver.TCPServer(("localhost", port), http.server.SimpleHTTPRequestHandler)
-            print(f"The web server is up at localhost:{port}")
+            print(f"localhost:{port} is up")
             global server_alive
             server_alive = True
             httpd.serve_forever()
@@ -42,8 +42,7 @@ def start_localhost():
 def main():
     os.chdir(exe_dir()) # Changes the directory to the executable
     os.chdir("../../../") # Uncomment for building macOS apps.
-    t1 = threading.Thread(target=start_localhost)
-    t1.start()
+    threading.Thread(target=start_web_server).start()
     while not server_alive:
         time.sleep(1)
     webbrowser.open(url=f"http://localhost:{str(port)}", new=1, autoraise=True)
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+"../Resources/ModestHost"
