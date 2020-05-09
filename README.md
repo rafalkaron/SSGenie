17784f6a69318797114cd7b9dde08c726dbbfcc1
rafalkaron <rafalkaron@gmail.com>
2020-05-09 18:46:59 +0200

    release

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # ModestHost
-*ModestHost enables you to quickly host files on a local web server.*  
-The default address of the local web server created by ModestHost `localhost:8000`. If the default port is already used, ModestHost uses consecutive port numbers to set up the server.
+*ModestHost enables you to quickly host files on a local web server.*
+
+## Operation
+The default address of the local web server created by ModestHost is `localhost:8000`. If the default port is already used, ModestHost uses consecutive port numbers to set up the server. Once the server is up, its address automatically opens in a new tab of your default web browser.
 
 ## Hosting Files on a Local Web Server
-*You can set up a local web server by running ModestHost in an appropriate directory.*
+*You can quickly host files on a local web server by running ModestHost in an appropriate directory.*
 
 ### Before you begin
 1. Download the latest version of **ModestHost** for your operating system. See [ModestHost Releases](https://github.com/rafalkaron/ModestHost/releases).
@@ -13,7 +15,7 @@ The default address of the local web server created by ModestHost `localhost:800
 1. Move the **ModestHost** executable to the directory with the files that you want to host on a local web server.
 1. Double-click **ModestHost**.
 1. If needed, accept every security prompt. See [Accepting macOS Security Prompts](https://github.com/rafalkaron/ModestHost/wiki/Accepting-macOS-Security-Prompts) or [Accepting Windows Security Prompts](https://github.com/rafalkaron/ModestHost/wiki/Accepting-Windows-Security-Prompts).  
-**Result:** Your default web browser opens and displays the files hosted on a local web server.
+**Result:** Your default web browser opens the local web server address in a new tab.
 
 ### Next steps
 * To finish, close **ModestHost**.
--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@
 """
 Quickly host files from the ModestHost directory on a localhost web server.
 """
-# Compile by using pyinstaller with the --onedir argument. Then, add the files from the compiled directory to the Platypus bundle and use the run.sh file as the Platypus script.
+# Compile by using PyInstaller with the --onedir argument. Then, in Platypus, add the compiled files to the bundle, use the "run.sh" file as the script, and select the "Remain running after execution" option.
 import http.server
 import socketserver
 import webbrowser
@@ -14,10 +14,10 @@ import time
 __version__ = "1.1"
 __author__ = "Rafał Karoń <rafalkaron@gmail.com>"
 
-server_alive = False
+server_up = False
 
 def exe_dir():
-    """Return the script or executable directory."""
+    """Return the executable directory."""
     if getattr(sys, 'frozen', False):
         exe_path = os.path.dirname(sys.executable)
     elif __file__:
@@ -25,6 +25,7 @@ def exe_dir():
     return exe_path
 
 def start_web_server():
+    """Start a local web server. Use port 8000 or higher."""
     global port
     port = 7999
     while True:
@@ -32,18 +33,18 @@ def start_web_server():
             port +=1
             httpd = socketserver.TCPServer(("localhost", port), http.server.SimpleHTTPRequestHandler)
             print(f"localhost:{port} is up")
-            global server_alive
-            server_alive = True
+            global server_up
+            server_up = True
             httpd.serve_forever()
             break
         except:
             continue        
 
 def main():
-    os.chdir(exe_dir()) # Changes the directory to the executable
+    os.chdir(exe_dir()) # Changes the directory to the executable directory.
     os.chdir("../../../") # Uncomment for building macOS apps.
     threading.Thread(target=start_web_server).start()
-    while not server_alive:
+    while not server_up:
         time.sleep(1)
     webbrowser.open(url=f"http://localhost:{str(port)}", new=1, autoraise=True)
 
