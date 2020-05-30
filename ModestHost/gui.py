2c26c931a929fa069a66597d81e491f2a64c9883
rafal.karon <45826759+rafalkaron@users.noreply.github.com>
2020-05-30 09:50:18 +0200

    gui

--- ModestHost/gui.py
+++ ModestHost/gui.py
@@ -6,31 +6,35 @@ import tkinter as tk
 window = tk.Tk()
 
 window.columnconfigure([0], minsize=150, weight=1)
-window.rowconfigure([1, 3], minsize=0, weight=1)
+window.rowconfigure([1, 2], minsize=3, weight=1)
 
 frm_input = tk.Frame(master=window)
-frm_input.grid(row=0, column=0, padx="10", pady="10", sticky="w")
+frm_input.grid(row=0, column=0, padx="10", pady="10", sticky="we")
 input_lbl = tk.Label(text="Directory to host:", master=frm_input)
 input_lbl.pack(side=tk.LEFT)
-ent_folder = tk.Entry(width="60", master=frm_input)
-ent_folder.pack(fill=tk.X)
+btn_browse = tk.Button(text="Browse...", master=frm_input)
+btn_browse.pack(side=tk.LEFT)
+ent_folder = tk.Entry(width="65", master=frm_input)
+ent_folder.pack(fill=tk.X, expand=True)
+
 
 frm_controls = tk.Frame(master=window)
-frm_controls.grid(row=1, column=0, padx="10", pady="10", sticky="n")
-chkbtn_preview = tk.Checkbutton(text="Open in a web browser", master=frm_controls)
-chkbtn_preview.select()
-chkbtn_preview.pack()
+frm_controls.grid(row=1, column=0, padx="10", pady="10", sticky="ns")
 btn_start = tk.Button(text="Start Server", height="2", master=frm_controls)
 btn_start.pack(side=tk.LEFT)
 btn_stop = tk.Button(text="Stop Server", height="2", master=frm_controls)
 btn_stop.pack(side=tk.LEFT)
 
 frm_status = tk.Frame(master=window)
-frm_status.grid(row=3, column=0, sticky="ws", padx="10", pady="10")
-lbl_status = tk.Label(text="Status:", master=frm_status)
-lbl_status.pack(side=tk.LEFT)
+frm_status.grid(row=3, column=0, sticky="we", padx="10", pady="10")
+chkbtn_preview = tk.Checkbutton(text="Open in a web browser", master=frm_status)
+chkbtn_preview.select()
+chkbtn_preview.pack(side=tk.LEFT)
 lbl_status_variable = tk.Label(text="localhost:8000 is up", master=frm_status)
-lbl_status_variable.pack(side=tk.LEFT)
+lbl_status_variable.pack(side=tk.RIGHT)
+lbl_status = tk.Label(text="Status:", master=frm_status)
+lbl_status.pack(side=tk.RIGHT)
+
 
 window.mainloop()
 
